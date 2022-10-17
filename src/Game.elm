c1ae631252ce458c645fc59160119e5214db9dad
Antoine Alberti <aa@antoinealberti.com>
2022-10-18 00:49:45 +0200

    Auto-mark

--- src/Game.elm
+++ src/Game.elm
@@ -3,65 +3,126 @@ module Game exposing (..)
 import Grid exposing (Coordinates, Grid)
 import Stack exposing (Stack)
 
+
 type alias Game =
-    { grid: Grid Cell
-    , toUndo: Stack GameRef
-    , toRedo: Stack GameRef
-    , toggleMode: ToggleMode
+    { grid : Grid Cell
+    , toUndo : Stack GameRef
+    , toRedo : Stack GameRef
+    , toggleMode : ToggleMode
     }
-type GameRef = GameRef Game
 
-type Hint = NoHint | CellsToMark Int
-type State = Marked | Unmarked | Unknown
-type Help = NoHelp | Error
+
+type GameRef
+    = GameRef Game
+
+
+type Hint
+    = NoHint
+    | CellsToMark Int
+
+
+type State
+    = Marked
+    | Unmarked
+    | Unknown
+
+
+type Help
+    = NoHelp
+    | Error
+
+
 type alias Cell =
-    { state: State
-    , hint: Hint
-    , help: Help
+    { state : State
+    , hint : Hint
+    , help : Help
     }
-type alias GridSize = { width: Int, height: Int }
-type ToggleMode = Toggling | Marking | Unmarking | Filling
 
-fromSize: GridSize -> Game
-fromSize {width, height} =
-    { grid = Grid.from(List.repeat height (List.repeat width { state = Unknown, hint = NoHint, help = NoHelp }))
+
+type alias GridSize =
+    { width : Int, height : Int }
+
+
+type ToggleMode
+    = Toggling
+    | Marking
+    | Unmarking
+    | Filling
+
+
+fromSize : GridSize -> Game
+fromSize { width, height } =
+    { grid = Grid.from (List.repeat height (List.repeat width { state = Unknown, hint = NoHint, help = NoHelp }))
     , toUndo = Stack.empty
     , toRedo = Stack.empty
     , toggleMode = Toggling
     }
 
+
 undo : Game -> Game
-undo game = case Stack.pop game.toUndo of
-    Just (GameRef previous) -> { previous | toRedo = Stack.push game.toRedo (GameRef game) }
-    Nothing -> game
+undo game =
+    case Stack.pop game.toUndo of
+        Just (GameRef previous) ->
+            { previous | toRedo = Stack.push game.toRedo (GameRef game) }
+
+        Nothing ->
+            game
+
 
 redo : Game -> Game
-redo game = case Stack.pop game.toRedo of
-    Just (GameRef next) -> next
-    Nothing -> game
+redo game =
+    case Stack.pop game.toRedo of
+        Just (GameRef next) ->
+            next
+
+        Nothing ->
+            game
+
 
 toggle : Game -> Coordinates -> Game
 toggle game coordinates =
     let
         markCell : Cell -> Cell
-        markCell cell = { cell | state = Marked }
+        markCell cell =
+            { cell | state = Marked }
 
-        unmarkCell  : Cell -> Cell
-        unmarkCell cell = { cell | state = Unmarked }
+        unmarkCell : Cell -> Cell
+        unmarkCell cell =
+            { cell | state = Unmarked }
 
         clearCell : Cell -> Cell
-        clearCell cell = { cell | state = Unknown }
+        clearCell cell =
+            { cell | state = Unknown }
 
-        toggleCell: Cell -> Cell
-        toggleCell cell = case cell.state of
-            Unknown -> markCell cell
-            Marked -> unmarkCell cell
-            Unmarked -> clearCell cell
+        toggleCell : Cell -> Cell
+        toggleCell cell =
+            case cell.state of
+                Unknown ->
+                    markCell cell
 
-        clearGridHelp: Grid Cell -> Grid Cell
-        clearGridHelp grid = Grid.replaceAll (\c -> {c|help = NoHelp}) grid
+                Marked ->
+                    unmarkCell cell
+
+                Unmarked ->
+                    clearCell cell
+
+        clearGridHelp : Grid Cell -> Grid Cell
+        clearGridHelp grid =
+            Grid.replaceAll (\c -> { c | help = NoHelp }) grid
     in
-        updateGrid game (Grid.update toggleCell game.grid coordinates |> clearGridHelp)
+    case game.toggleMode of
+        Toggling ->
+            updateGrid game (Grid.update toggleCell game.grid coordinates |> clearGridHelp)
+
+        Marking ->
+            updateGrid game (Grid.update markCell game.grid coordinates |> clearGridHelp)
+
+        Unmarking ->
+            updateGrid game (Grid.update unmarkCell game.grid coordinates |> clearGridHelp)
+
+        Filling ->
+            updateGrid game (fill game.grid coordinates |> clearGridHelp)
+
 
 updateHint : Game -> Coordinates -> String -> Game
 updateHint game coordinates hint =
@@ -69,94 +130,177 @@ updateHint game coordinates hint =
         updateCellHint : String -> Cell -> Cell
         updateCellHint h cell =
             { cell
-            | hint = String.toInt h
-                |> Maybe.map CellsToMark
-                |> Maybe.withDefault NoHint
+                | hint =
+                    String.toInt h
+                        |> Maybe.map CellsToMark
+                        |> Maybe.withDefault NoHint
             }
     in
-        updateGrid game (Grid.update (updateCellHint hint) game.grid coordinates)
+    updateGrid game (Grid.update (updateCellHint hint) game.grid coordinates)
+
 
 isOver : Game -> Bool
 isOver game =
     let
         isHintUnverified : Grid Cell -> Coordinates -> Cell -> Bool
-        isHintUnverified grid coordinates cell = case cell.hint of
-            NoHint -> False
-            CellsToMark cellsToMark -> (numberOfMarkedNeighbours grid coordinates) /= cellsToMark
+        isHintUnverified grid coordinates cell =
+            case cell.hint of
+                NoHint ->
+                    False
+
+                CellsToMark cellsToMark ->
+                    numberOfMarkedNeighbours grid coordinates /= cellsToMark
     in
-        case Grid.count isUnknown game.grid of
-            0 -> (Grid.indexedCount (isHintUnverified game.grid) game.grid) == 0
-            _ -> False
+    case Grid.count isUnknown game.grid of
+        0 ->
+            Grid.indexedCount (isHintUnverified game.grid) game.grid == 0
+
+        _ ->
+            False
+
 
 isUnknown : Cell -> Bool
-isUnknown cell = case cell.state of
-    Unknown -> True
-    _ -> False
+isUnknown cell =
+    case cell.state of
+        Unknown ->
+            True
+
+        _ ->
+            False
+
 
 numberOfMarkedNeighbours : Grid Cell -> Coordinates -> Int
 numberOfMarkedNeighbours grid coordinates =
     let
         isCellMarked : Cell -> Bool
-        isCellMarked cell = case cell.state of
-            Marked -> True
-            _ -> False
+        isCellMarked cell =
+            case cell.state of
+                Marked ->
+                    True
+
+                _ ->
+                    False
     in
-        Grid.neighbours grid coordinates
-            |> Grid.count isCellMarked
+    Grid.neighbours grid coordinates
+        |> Grid.count isCellMarked
+
 
 numberOfUnmarkedNeighbours : Grid Cell -> Coordinates -> Int
 numberOfUnmarkedNeighbours grid coordinates =
     let
         isCellUnmarked : Cell -> Bool
-        isCellUnmarked cell = case cell.state of
-            Unmarked -> True
-            _ -> False
+        isCellUnmarked cell =
+            case cell.state of
+                Unmarked ->
+                    True
+
+                _ ->
+                    False
     in
-        Grid.neighbours grid coordinates
-            |> Grid.count isCellUnmarked
+    Grid.neighbours grid coordinates
+        |> Grid.count isCellUnmarked
+
+
+numberOfUnknownNeighbours : Grid Cell -> Coordinates -> Int
+numberOfUnknownNeighbours grid coordinates =
+    let
+        isCellUnknown : Cell -> Bool
+        isCellUnknown cell =
+            case cell.state of
+                Unknown ->
+                    True
+
+                _ ->
+                    False
+    in
+    Grid.neighbours grid coordinates
+        |> Grid.count isCellUnknown
+
 
 updateGrid : Game -> Grid Cell -> Game
 updateGrid game grid =
     { game
-    | grid = grid
-    , toUndo = Stack.push game.toUndo (GameRef game)
-    , toRedo = Stack.empty
+        | grid = grid
+        , toUndo = Stack.push game.toUndo (GameRef game)
+        , toRedo = Stack.empty
     }
+
 
 clearMarks : Game -> Game
 clearMarks game =
     let
         clearMark : Cell -> Cell
-        clearMark cell = { cell | state = Unknown, help = NoHelp }
+        clearMark cell =
+            { cell | state = Unknown, help = NoHelp }
     in
-        { game
+    { game
         | grid = Grid.replaceAll clearMark game.grid
         , toUndo = Stack.push game.toUndo (GameRef game)
         , toRedo = Stack.empty
-        }
+    }
+
+
+fill : Grid Cell -> Coordinates -> Grid Cell
+fill grid coordinates =
+    let
+        cellHint : Grid Cell -> Coordinates -> Hint
+        cellHint g c =
+            Grid.get g c
+                |> Maybe.map (\cell -> cell.hint)
+                |> Maybe.withDefault NoHint
+
+        markUnknown : Cell -> Cell
+        markUnknown cell =
+            case cell.state of
+                Unknown ->
+                    { cell | state = Marked }
+
+                _ ->
+                    cell
+    in
+    case cellHint grid coordinates of
+        NoHint ->
+            grid
+
+        CellsToMark cellsToMark ->
+            if cellsToMark == numberOfUnknownNeighbours grid coordinates + numberOfMarkedNeighbours grid coordinates then
+                Grid.replaceNeighbours markUnknown grid coordinates
+
+            else
+                grid
+
 
 setHelp : Game -> Game
 setHelp game =
     let
         isInError : Grid Cell -> Coordinates -> Int -> Bool
         isInError grid coordinates cellsToMark =
-            cellsToMark < numberOfMarkedNeighbours grid coordinates
-            || cellsToMark > (Grid.neighbours grid coordinates |> Grid.count (\_ -> True)) - numberOfUnmarkedNeighbours grid coordinates
+            cellsToMark
+                < numberOfMarkedNeighbours grid coordinates
+                || cellsToMark
+                > (Grid.neighbours grid coordinates |> Grid.count (\_ -> True))
+                - numberOfUnmarkedNeighbours grid coordinates
 
         setCellHelp : Grid Cell -> Coordinates -> Cell -> Cell
-        setCellHelp grid coordinates cell = case cell.hint of
-            NoHint -> { cell | help = NoHelp }
-            CellsToMark cellsToMark ->
-                if isInError grid coordinates cellsToMark then
-                    { cell | help = Error }
-                else
+        setCellHelp grid coordinates cell =
+            case cell.hint of
+                NoHint ->
                     { cell | help = NoHelp }
+
+                CellsToMark cellsToMark ->
+                    if isInError grid coordinates cellsToMark then
+                        { cell | help = Error }
+
+                    else
+                        { cell | help = NoHelp }
     in
-        { game
+    { game
         | grid = Grid.indexedReplaceAll (setCellHelp game.grid) game.grid
         , toUndo = Stack.push game.toUndo (GameRef game)
         , toRedo = Stack.empty
-        }
+    }
+
 
 changeToggleMode : Game -> ToggleMode -> Game
-changeToggleMode game toggleMode = { game | toggleMode = toggleMode}
+changeToggleMode game toggleMode =
+    { game | toggleMode = toggleMode }
--- src/Grid.elm
+++ src/Grid.elm
@@ -2,85 +2,110 @@ module Grid exposing (..)
 
 import Array exposing (Array)
 
+
 type alias Coordinates =
-    { x: Int
-    , y: Int
+    { x : Int
+    , y : Int
     }
-type alias Grid a = Array (Row a)
-type alias Row a = Array a
+
+
+type alias Grid a =
+    Array (Row a)
+
+
+type alias Row a =
+    Array a
+
 
 from : List (List a) -> Grid a
-from rows = List.map (Array.fromList) rows |> Array.fromList
+from rows =
+    List.map Array.fromList rows |> Array.fromList
+
 
 indexedRowsMap : (Int -> Row a -> b) -> Grid a -> List b
-indexedRowsMap f grid = Array.indexedMap f grid |> Array.toList
+indexedRowsMap f grid =
+    Array.indexedMap f grid |> Array.toList
+
 
 indexedCellsMap : (Int -> a -> b) -> Row a -> List b
-indexedCellsMap f row = Array.indexedMap f row |> Array.toList
+indexedCellsMap f row =
+    Array.indexedMap f row |> Array.toList
+
 
 update : (a -> a) -> Grid a -> Coordinates -> Grid a
-update f grid coordinates = get grid coordinates
-    |> Maybe.map (f)
-    |> Maybe.map (set grid coordinates)
-    |> Maybe.withDefault grid
+update f grid coordinates =
+    get grid coordinates
+        |> Maybe.map f
+        |> Maybe.map (set grid coordinates)
+        |> Maybe.withDefault grid
+
 
 count : (a -> Bool) -> Grid a -> Int
 count predicate grid =
     let
         countCells : (a -> Bool) -> Row a -> Int
-        countCells p row = Array.toList row
-            |> List.filter p
-            |> List.length
+        countCells p row =
+            Array.toList row
+                |> List.filter p
+                |> List.length
     in
-        Array.toList grid
-            |> List.map (countCells predicate)
-            |> List.sum
+    Array.toList grid
+        |> List.map (countCells predicate)
+        |> List.sum
+
 
 indexedCount : (Coordinates -> a -> Bool) -> Grid a -> Int
 indexedCount predicate grid =
     let
         indexedCellsCount : (Coordinates -> a -> Bool) -> Int -> Row a -> Int
         indexedCellsCount p y row =
-            Array.indexedMap (\x a -> p {x = x, y = y} a) row
+            Array.indexedMap (\x a -> p { x = x, y = y } a) row
                 |> Array.filter (\a -> a)
                 |> Array.toList
                 |> List.length
     in
-        Array.indexedMap (indexedCellsCount predicate) grid
-            |> Array.toList
-            |> List.sum
+    Array.indexedMap (indexedCellsCount predicate) grid
+        |> Array.toList
+        |> List.sum
 
-get: Grid a -> Coordinates -> Maybe a
+
+get : Grid a -> Coordinates -> Maybe a
 get grid coordinates =
     let
         rowCell : Int -> Row a -> Maybe a
-        rowCell x row = Array.get x row
+        rowCell x row =
+            Array.get x row
     in
-        Array.get coordinates.y grid |> Maybe.andThen (rowCell coordinates.x)
+    Array.get coordinates.y grid |> Maybe.andThen (rowCell coordinates.x)
+
 
 set : Grid a -> Coordinates -> a -> Grid a
 set grid coordinates cell =
     let
         setRowToGrid : Int -> Grid a -> Row a -> Grid a
-        setRowToGrid y g row = Array.set y row g
+        setRowToGrid y g row =
+            Array.set y row g
 
         setCellToRow : Int -> a -> Row a -> Row a
-        setCellToRow x c row = Array.set x c row
+        setCellToRow x c row =
+            Array.set x c row
     in
-        Array.get coordinates.y grid
-            |> Maybe.map (setCellToRow coordinates.x cell)
-            |> Maybe.map (setRowToGrid coordinates.y grid)
-            |> Maybe.withDefault grid
+    Array.get coordinates.y grid
+        |> Maybe.map (setCellToRow coordinates.x cell)
+        |> Maybe.map (setRowToGrid coordinates.y grid)
+        |> Maybe.withDefault grid
+
 
 neighbours : Grid a -> Coordinates -> Grid a
-neighbours grid coordinates = from
-    (
-        [ line grid coordinates (coordinates.y - 1)
-        , line grid coordinates coordinates.y
-        , line grid coordinates (coordinates.y + 1)
-        ]
-        |> List.filter (\list -> not (List.isEmpty list))
-    )
+neighbours grid coordinates =
+    from
+        ([ line grid coordinates (coordinates.y - 1)
+         , line grid coordinates coordinates.y
+         , line grid coordinates (coordinates.y + 1)
+         ]
+            |> List.filter (\list -> not (List.isEmpty list))
+        )
+
 
 line : Grid a -> Coordinates -> Int -> List a
 line grid coordinates index =
@@ -88,29 +113,68 @@ line grid coordinates index =
         subArray : Int -> Int -> Array a -> Array a
         subArray start end array =
             let
-                s = if start < 0 then
+                s =
+                    if start < 0 then
                         0
+
                     else
                         start
-                e = if end < Array.length array then
+
+                e =
+                    if end < Array.length array then
                         end
+
                     else
-                        (Array.length array) - 1
+                        Array.length array - 1
             in
-                Array.slice s (e+1) array
+            Array.slice s (e + 1) array
     in
-        Array.get index grid
-            |> Maybe.withDefault (Array.fromList [])
-            |> subArray (coordinates.x - 1) (coordinates.x + 1)
-            |> Array.toList
+    Array.get index grid
+        |> Maybe.withDefault (Array.fromList [])
+        |> subArray (coordinates.x - 1) (coordinates.x + 1)
+        |> Array.toList
 
-replaceAll : (a -> a) -> Grid a -> Grid a
-replaceAll map grid = Array.map (\row -> Array.map map row) grid
 
-indexedReplaceAll : (Coordinates -> a -> a) -> Grid a -> Grid a
+replaceAll : (a -> b) -> Grid a -> Grid b
+replaceAll map grid =
+    Array.map (\row -> Array.map map row) grid
+
+
+indexedReplaceAll : (Coordinates -> a -> b) -> Grid a -> Grid b
 indexedReplaceAll cellMap grid =
     let
-        rowIndexReplaceAll : (Coordinates -> a -> a) -> Int -> Row a -> Row a
-        rowIndexReplaceAll cMap y row = Array.indexedMap (\x cell -> cMap {x=x,y=y} cell) row
+        rowIndexReplaceAll : (Coordinates -> a -> b) -> Int -> Row a -> Row b
+        rowIndexReplaceAll cMap y row =
+            Array.indexedMap (\x cell -> cMap { x = x, y = y } cell) row
     in
-        Array.indexedMap (rowIndexReplaceAll cellMap) grid
+    Array.indexedMap (rowIndexReplaceAll cellMap) grid
+
+
+replaceNeighbours : (a -> a) -> Grid a -> Coordinates -> Grid a
+replaceNeighbours mapper grid coordinates =
+    let
+        isNeighbour : Coordinates -> Coordinates -> Bool
+        isNeighbour center cell =
+            cell.x
+                >= center.x
+                - 1
+                && cell.x
+                <= center.x
+                + 1
+                && cell.y
+                >= center.y
+                - 1
+                && cell.y
+                <= center.y
+                + 1
+
+        mapperForNeighbours : (a -> a) -> Coordinates -> (Coordinates -> a -> a)
+        mapperForNeighbours m center =
+            \cellCoordinates cell ->
+                if isNeighbour center cellCoordinates then
+                    m cell
+
+                else
+                    cell
+    in
+    indexedReplaceAll (mapperForNeighbours mapper coordinates) grid
